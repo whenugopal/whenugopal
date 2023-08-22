e062359cfe67c614023c7f0e256aa64cb3cea356
vg7venu <vg7venu@gmail.com>
2023-08-22 22:58:34 +0530

    feat: add device inventory

---
+++ inventory/devices.js
@@ -0,0 +1,161 @@
+const mobiles = [
+  {
+    name: "google-pixel-5",
+    width: 393,
+    height: 851,
+  },
+  {
+    name: "samsung-galaxy-s21-ultra",
+    width: 360,
+    height: 760,
+  },
+  {
+    name: "oneplus-9-pro",
+    width: 412,
+    height: 888,
+  },
+  {
+    name: "xiaomi-mi-11",
+    width: 360,
+    height: 800,
+  },
+  {
+    name: "sony-xperia-1-iii",
+    width: 384,
+    height: 1644,
+  },
+  {
+    name: "iPhone-12-Pro-Max",
+    width: 428,
+    height: 926,
+  },
+  {
+    name: "iPhone-12-Pro",
+    width: 390,
+    height: 844,
+  },
+  {
+    name: "iPhone-12",
+    width: 390,
+    height: 844,
+  },
+  {
+    name: "iPhone-12-Mini",
+    width: 360,
+    height: 780,
+  },
+  {
+    name: "iPhone-SE-2",
+    width: 375,
+    height: 667,
+  },
+  {
+    name: "iPhone-11-Pro-Max",
+    width: 414,
+    height: 896,
+  },
+  {
+    name: "iPhone-11-Pro",
+    width: 375,
+    height: 812,
+  },
+  {
+    name: "iPhone-11",
+    width: 414,
+    height: 896,
+  },
+  {
+    name: "iPhone-XS-Max",
+    width: 414,
+    height: 896,
+  },
+  {
+    name: "iPhone-XR",
+    width: 414,
+    height: 896,
+  },
+  {
+    name: "iPhone-X",
+    width: 375,
+    height: 812,
+  },
+  {
+    name: "iPhone-8-Plus",
+    width: 414,
+    height: 736,
+  },
+  {
+    name: "iPhone-8",
+    width: 375,
+    height: 667,
+  },
+];
+
+const desktop = [
+  {
+    name: "Desktop-Full-HD",
+    year: "2010s",
+    width: 1920,
+    height: 1080,
+  },
+  {
+    name: "Desktop-4K-Ultra-HD",
+    year: "2010s",
+    width: 3840,
+    height: 2160,
+  },
+  {
+    name: "iMac-27-inch-5K-Retina",
+    year: "2010s",
+    width: 5120,
+    height: 2880,
+  },
+  {
+    name: "Dell-Ultrasharp-U3415W",
+    year: "2010s",
+    width: 3440,
+    height: 1440,
+  },
+  {
+    name: "LG-38WN95C-W",
+    year: "2020",
+    width: 3840,
+    height: 1600,
+  },
+];
+
+const tablets = [
+  {
+    name: "iPad-Pro-12-9-inch-",
+    width: 1024,
+    height: 1366,
+  },
+  {
+    name: "iPad-Pro-11-inch",
+    width: 834,
+    height: 1194,
+  },
+  {
+    name: "iPad-Air-10-9-inch",
+    width: 820,
+    height: 1180,
+  },
+  {
+    name: "Samsung-Galaxy-Tab-S7+",
+    width: 1600,
+    height: 2560,
+  },
+  {
+    name: "Microsoft-Surface-Pro-7",
+    width: 1080,
+    height: 1920,
+  },
+];
+
+const devices = {
+  mobiles: mobiles,
+  desktop: desktop,
+  tablets: tablets,
+};
+
+export default devices;
