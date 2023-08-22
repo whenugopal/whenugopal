46e1ed01c34d7e01574e2eb6356be6eeaa4c5277
vg7venu <vg7venu@gmail.com>
2023-08-22 23:22:11 +0530

    fix: add some urls

--- test/namasteTest.js
+++ test/namasteTest.js
@@ -2,7 +2,7 @@ import namaste from "../index.js";
 
 console.log(
   namaste({
-    urls: ["https://www.google.com", "https://www.wiley.com"],
+    urls: ["https://www.google.com", "https://www.apple.com"],
     deviceName: "mobiles",
   })
 );
